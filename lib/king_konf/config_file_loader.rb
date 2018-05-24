82fc4b94b2bde6d11de333699c6af64cc11fb713
Kristoffer Hansen <khansen@zendesk.com>
2018-05-24 16:19:28 +0200

    Use `YAML.load` to load configuration file
    Using `.load` might be vulnerable if you're loading user generated YAML
    file. Most use cases of this gem would only read developer generated
    YAML.
    
    We're switching away from `.safe_load` to increase compatibility for
    e.g. Rails 3 and 4 applications that use `Syck` to parse YAML.

--- lib/king_konf/config_file_loader.rb
+++ lib/king_konf/config_file_loader.rb
@@ -11,9 +11,7 @@ module KingKonf
       # First, load the ERB template from disk.
       template = ERB.new(File.new(path).read)
 
-      # The last argument to `safe_load` allows us to use aliasing to share
-      # configuration between environments.
-      data = YAML.safe_load(template.result(binding), [], [], true)
+      data = YAML.load(template.result(binding))
 
       # Grab just the config for the environment, if specified.
       data = data.fetch(environment) unless environment.nil?
